57344a3ab022e4d0a98dfb0211804ac03a02bf58
Yoni Kohn <yonikohn@Yonis-MacBook-Pro.local>
2020-11-23 15:14:14 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notess-app-upload",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://1c7wk1f3pi.execute-api.us-east-1.amazonaws.com/prod/",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_HW00FHKo0",
+      APP_CLIENT_ID: "3uj7c945vdqk13m8anjjtsn5aa",
+      IDENTITY_POOL_ID: "us-east-1:5c3a8551-53fd-4d2c-8692-c312d4f83ee7",
+    },
+  };
+  
+  export default config;
